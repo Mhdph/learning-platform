d9afa9eaa741d35bf4e6abb52033264ad558a88b
Mahdi Parivash <mahdiparivash@protonmail.com>
2024-05-02 23:15:20 +0330

    feat:initial project

---
+++ src/app.d.ts
@@ -0,0 +1,17 @@
+// See https://kit.svelte.dev/docs/types#app
+// for information about these interfaces
+import PocketBase, { type AuthModel } from 'pocketbase';
+declare global {
+	namespace App {
+		// interface Error {}
+		interface Locals {
+			pb: PocketBase;
+			user: AuthModel | undefined;
+		}
+		// interface PageData {}
+		// interface PageState {}
+		// interface Platform {}
+	}
+}
+
+export {};
