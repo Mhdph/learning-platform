08a36be494bd0b1a01d818e2a4faff38f1f59eba
Mahdi Parivash <mahdiparivash@protonmail.com>
2024-05-02 23:15:28 +0330

    feat:add auth pages

---
+++ src/routes/(auth)/+layout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+</script>
+
+<div class="h-screen w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
+	<div class="flex items-center justify-center py-12">
+		<slot />
+	</div>
+	<div class="hidden bg-muted lg:block">
+		<img
+			src="/images/placeholder.svg"
+			alt="placeholder"
+			width="1920"
+			height="1080"
+			class="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+		/>
+	</div>
+</div>
